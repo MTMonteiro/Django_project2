9cbd5444bfe7f4c32c96728486e786ef4b4f5031
MTMonteiro <matheusmonteiroalves@id.uff.br>
2020-04-17 23:21:15 -0300

    1.0

--- django2/settings.py
+++ django2/settings.py
@@ -137,8 +137,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Salvar midia no local correto
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #conf email
 #para teste ou quando não tem um servidor de email
